b282d86912c7bdc1eb9f03734dd17f0b9132ea76
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-12 07:06:40 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9,6 +9,13 @@ var SceneOne = new Phaser.Class({
     },
     create: function() {
         this.plane = this.add.image(400, 300, "coin");
+        this.time.addEvent({
+        delay: 3000,
+        loop: false,
+        callback: () => {
+            this.scene.start("SceneTwo");
+        }
+    })
     },
     update: function() {}
 });
