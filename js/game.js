2a06f41c4b2ed28103e505d1f9c93d7e7f8128a8
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-11 21:02:02 -0400

    Create game.js

---
+++ js/game.js
@@ -0,0 +1,216 @@
+export default class Game extends Phaser.Scene {
+  constructor() {
+    super('game')
+  }
+
+function preload() {
+     var progressBar = this.add.graphics();
+            var progressBox = this.add.graphics();
+            progressBox.fillStyle(0x222222, 0.8);
+            progressBox.fillRect(240, 270, 320, 50);
+            
+            var width = this.cameras.main.width;
+            var height = this.cameras.main.height;
+            var loadingText = this.make.text({
+                x: width / 2,
+                y: height / 2 - 50,
+                text: 'Loading...',
+                style: {
+                    font: '20px monospace',
+                    fill: '#ffffff'
+                }
+            });
+            loadingText.setOrigin(0.5, 0.5);
+            
+            var percentText = this.make.text({
+                x: width / 2,
+                y: height / 2 - 5,
+                text: '0%',
+                style: {
+                    font: '18px monospace',
+                    fill: '#ffffff'
+                }
+            });
+            percentText.setOrigin(0.5, 0.5);
+            
+            var assetText = this.make.text({
+                x: width / 2,
+                y: height / 2 + 50,
+                text: '',
+                style: {
+                    font: '18px monospace',
+                    fill: '#ffffff'
+                }
+            });
+            assetText.setOrigin(0.5, 0.5);
+            
+            this.load.on('progress', function (value) {
+                percentText.setText(parseInt(value * 100) + '%');
+                progressBar.clear();
+                progressBar.fillStyle(0xffffff, 1);
+                progressBar.fillRect(250, 280, 300 * value, 30);
+            });
+            
+            this.load.on('fileprogress', function (file) {
+                assetText.setText('Loading asset: ' + file.key);
+            });
+
+            this.load.on('complete', function () {
+                progressBar.destroy();
+                progressBox.destroy();
+                loadingText.destroy();
+                percentText.destroy();
+                assetText.destroy();
+            });
+            
+            this.load.image('logo', 'logo.png');
+            for (var i = 0; i < 0; i++) {
+                this.load.image('logo'+i, 'logo.png');
+            }
+this.load.atlas('player', 'character/spritesheet.png', 'character/spritesheet.json');
+this.load.image('coin','blocks/coin.png');
+this.load.image('spike','blocks/spike.png');
+this.load.image('dirt','blocks/dirt.png');
+this.load.image('bottom','blocks/bottom.png');
+this.load.image('edgel','blocks/edge.png');
+this.load.image('edger', 'blocks/edger.png');
+this.load.image('platform','blocks/platform.png');
+this.load.audio('music','audio/backgroundmusic.mp3');
+this.load.audio('collectcoin', 'audio/collectcoin.wav');
+this.load.audio('complete','audio/complete.mp3');
+this.load.audio('hurt', 'audio/Hit.wav');
+this.load.audio('jump', 'audio/Jump.wav');
+}
+function create(){
+var music = this.sound.add('music');
+music.setLoop(true);
+music.play();
+	this.cameras.main.setBackgroundColor('#ffffff');
+	this.spawnPlayer = (x, y)=>{
+		this.player = this.physics.add.sprite(x, y, "player", "1");
+		this.player.body.setGravityY(800);
+		this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.player, this.bottom);
+    this.physics.add.collider(this.player, this.edgel);
+    this.physics.add.collider(this.player, this.edger);
+    this.physics.add.collider(this.player, this.dirt);
+		this.cameras.main.startFollow(this.player);
+		this.player.score = 0;
+    this.player.score = parseInt(localStorage.getItem('score')) || 0;
+		this.scoreText = this.add.text(0, 0, "Score: "+this.player.score, {
+			fill:"#000000",
+			fontSize:"15px",
+			fontFamily:"Arial Black"
+		}).setScrollFactor(0).setDepth(200);
+	};
+	this.collectCoin = (player, coin)=>{
+		player.score+=10;
+    this.sound.play('collectcoin');
+		coin.destroy();
+		this.scoreText.setText("Score: "+ this.player.score);
+    localStorage.setItem('score',this.player.score);
+    if (this.player.score == 500){
+      this.sound.play('complete'); 
+    }else if(this.player.score==1000){
+      this.sound.play('complete');
+    }else if(this.player.score==1500){
+      this.sound.play('complete');
+    }else if(this.player.score===2000){
+      this.sound.play('complete');
+    }
+	};
+	this.die = ()=>{
+		this.physics.pause();
+    this.player.score-=100;
+    localStorage.setItem('score',this.player.score);
+    this.sound.play('hurt');
+		let deathText = this.add.text(0, 0, "YOU DIED", {
+			color:"#d53636",
+			fontFamily:"Arial Black",
+			fontSize:"50px"
+		}).setScrollFactor(0);
+		Phaser.Display.Align.In.Center(deathText, this.add.zone(400, 250, 800, 500));
+	};
+	this.platforms = this.physics.add.staticGroup();
+  this.bottom = this.physics.add.staticGroup();
+  this.edger = this.physics.add.staticGroup();
+  this.dirt = this.physics.add.staticGroup();
+  this.edgel = this.physics.add.staticGroup();
+	this.coins = this.physics.add.group();
+	this.spikes = this.physics.add.group();
+	let mapArr = map.split('.');
+	let drawX = 0;
+	let drawY = 0;
+		mapArr.forEach(row=>{
+		drawX = 0;
+		for(let i = 0; i<row.length; i++){
+			if(row.charAt(i)==='p'){
+				this.platforms.create(drawX, drawY, "platform");
+			}else if(row.charAt(i)==='2'){
+				if(row.charAt(i+1)==='p'){
+					this.spawnPlayer(drawX-4, drawY-12);
+				}else if(row.charAt(i-1)==='p'){
+					this.spawnPlayer(drawX+4, drawY-12);
+				}else{
+					this.spawnPlayer(drawX, drawY-12);					
+				}
+			}else if(row.charAt(i)==='c'){
+				this.coins.create(drawX, drawY+1, "coin");
+			}else if(row.charAt(i)==='s'){
+				this.spikes.create(drawX, drawY+1, "spike");
+			}else if(row.charAt(i)==='d'){
+        this.dirt.create(drawX,drawY, 'dirt');
+      }else if(row.charAt(i)==='l'){
+        this.edgel.create(drawX,drawY, 'edgel');
+      }else if(row.charAt(i)==='b'){
+        this.bottom.create(drawX,drawY, 'bottom');
+      }else if(row.charAt(i)==='r'){
+        this.bottom.create(drawX,drawY, 'edger');
+      }
+			drawX+=18;
+		}
+		drawY+=18;
+	});
+	this.physics.add.overlap(this.player, this.coins, this.collectCoin, null, this);
+	this.physics.add.overlap(this.player, this.spikes, this.die, null, this);
+	this.anims.create({
+		key:"walk",
+			frames:[{key:"player", frame:"1"}, {key:"player", frame:"0"}],
+			frameRate:10,
+			repeat:-1
+	});
+	this.anims.create({
+		key:"stand",
+			frames:[{key:"player", frame:"1"}],
+			frameRate:1
+	});
+	this.key_UP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+	this.key_LEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+	this.key_RIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+}
+
+function update() {
+	if(this.key_UP.isDown && this.player.body.touching.down){
+		this.player.setVelocityY(-350);
+    this.sound.play('jump');
+	}
+	if(this.key_LEFT.isDown){
+		this.player.setVelocityX(-200);
+		this.player.anims.play("walk", true);
+		this.player.flipX = true;
+	}else if(this.key_RIGHT.isDown){
+		this.player.setVelocityX(200);
+		this.player.anims.play("walk", true);
+		this.player.flipX = false;
+	}else{
+		this.player.anims.play("stand", true);
+		this.player.setVelocityX(0);
+	}
+}
+var reset = 0; 
+function resetScore(){
+  alert("You can't undo this! Reload now to save your stuff")
+  localStorage.setItem('score',reset);
+  console.log('Player/User Score Succsessfully Reset');
+};
+}
