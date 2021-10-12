40ab15ea8f4aa575ddfff3dd5632336ac2155235
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-12 19:36:05 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -7,9 +7,9 @@ var Menu = new Phaser.Class({
     preload: function() {},
     create: function() {
     this.add.text(300, 60, 'That New Platformer Game', {fill: '#0f0'})
-    this.clickButton = this.add.text(330, 150, 'Default Map', { fill: '#0f0'})
+    this.clickButton = this.add.text(260, 150, 'Original Map', { fill: '#0f0'})
      .setInteractive()
-     .on('pointerdown', () => this.scene.start("Original Map") )
+     .on('pointerdown', () => this.scene.start("DefaultMap") )
     this.clickMap2 = this.add.text(310, 200, "GrantM's Map", { fill: '#0f0'})
      .setInteractive()
      .on('pointerdown', () => this.scene.start("SecMap") )
