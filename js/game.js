81e413865332f940d58bf21f18c95cfcdf275556
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-12 10:09:00 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -106,7 +106,12 @@ music.play();
 			fontFamily:"Arial Black"
 		}).setScrollFactor(0).setDepth(200);
 	};
-	    this.levelText = this.add.text(0,0, "Level: "+this.player.level, {fill:"#000000", fontSize: "15px", fontFaimly: "Arial Black"}).setScrollFactor(0).setDepth(200);
+	    this.levelText = this.add.text(0,0, "Level: "+this.player.level, {
+		    fill:"#000000",
+		    fontSize: "15px",
+		    fontFaimly: "Arial Black"
+	    }).setScrollFactor(0).setDepth(200);
+       };
 	this.collectCoin = (player, coin)=>{
 		player.score+=10;
     this.sound.play('collectcoin');
