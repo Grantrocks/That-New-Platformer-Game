9d5136314355032ddc750eb92d9f239bd4f5ec59
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-12 09:11:35 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const phaserConfig = {
             debug: false
         }
     },
-    scene: [ SceneOne, SceneTwo ]
+    scene: [ DefaultMap, SceneTwo ]
 };
             const game = new Phaser.Game(phaserConfig);
 
