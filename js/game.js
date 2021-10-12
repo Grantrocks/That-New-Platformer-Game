ef985d37383c552561bdbcc1c9ef135e41e986a0
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-12 16:53:20 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -115,7 +115,7 @@ music.play();
 		    	fontSize:"15px",
 		    	fontFaimly:"Arial Black"
 	    	}).setScrollFactor(0).setDepth(200);
-		this.progressText = this.add.text(200,0, "Level Up in: "+this.player.progressl, {
+		this.progresslText = this.add.text(200,0, "Level Up in: "+this.player.progressl, {
 		   	fill:"#000000",
 		    	fontSize:"15px",
 		    	fontFaimly:"Arial Black"
@@ -127,11 +127,11 @@ music.play();
     this.sound.play('collectcoin');
 		coin.destroy();
 		this.scoreText.setText("Score: "+ this.player.score);
-		this.progressText.setText("Level Up in: "+this.player.progressl);
+		this.progresslText.setText("Level Up in: "+this.player.progressl);
     localStorage.setItem('score',this.player.score);
     localStorage.setItem('progress',this.player.progress);
     localStorage.setItem('progressl',this.player.progressl);
-    if (this.player.progress == (this.player.level+500){
+    if (this.player.progress == 500{
       this.sound.play('complete');
       this.player.level += 1;
       this.levelText.setText("Level: "+this.player.level);
