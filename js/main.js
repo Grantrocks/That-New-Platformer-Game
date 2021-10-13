18b5c103beedd6ace78256f8c3f1e089d7914fab
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-13 13:21:29 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,24 +14,6 @@ const phaserConfig = {
     scene: [ Menu, DefaultMap, SecMap ]
 };
             const game = new Phaser.Game(phaserConfig);
-function create(){
-    window.addEventListener('resize', resize);
-    resize();
-
-    // Place the remaining create function code below
-}
-function resize() {
-        var canvas = game.canvas, width = document.getElementsByClassName("parent-container")[0].offsetWidth, height = window.innerHeight;
-    var wratio = width / height, ratio = canvas.width / canvas.height;
-
-    if (wratio < ratio) {
-        canvas.style.width = width + 'px';
-        canvas.style.height = (width / ratio) + 'px';
-    } else {
-        canvas.style.width = (height * ratio) + 'px';
-        canvas.style.height = height + 'px';
-    }
-}
 /*var config = {
     type: Phaser.AUTO,
     parent: 'game',
