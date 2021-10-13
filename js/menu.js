e05e10f2d3c11308c26d25c5df3b6ee687001488
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-13 12:25:30 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,12 +6,11 @@ var Menu = new Phaser.Class({
     init: function() {},
     preload: function() {},
     create: function() {
-    var button = this.add.text(0, 0, 'Repositorie').setInteractive();
+    var button = this.add.text(0, 0, 'Repository', {fill: '#0f0'}).setInteractive();
 
     button.on('pointerup', openExternalLink, this);
 
     this.add.text(300, 60, 'That New Platformer Game', {fill: '#0f0'})
-    this.add.text(300, 90, 'A Game Made By Grant M', {fill: '#0f0'})
     this.add.text(300, 90, 'A Game Made By Grant M', {fill: '#0f0'})
 
     this.clickButton = this.add.text(260, 150, 'Original Map', { fill: '#0f0'})
