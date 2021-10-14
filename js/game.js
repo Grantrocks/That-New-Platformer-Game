e5e1641edb8c56dc1f91b08847f06c41716b99ce
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-14 19:25:31 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -88,7 +88,6 @@ this.load.audio('jump', 'audio/Jump.wav');
         var music = this.sound.add('music');
 music.setLoop(true);
 music.play();
-	// global variables
 	this.cameras.main.setBackgroundColor('#ffffff');
 	this.spawnPlayer = (x, y)=>{
 		this.player = this.physics.add.sprite(x, y, "player", "1");
@@ -134,9 +133,7 @@ music.play();
     localStorage.setItem('score',this.player.score);
     localStorage.setItem('progress',this.player.progress);
     localStorage.setItem('progressl',this.player.progressl);
-
-	};
-if (this.player.progress >= 800){
+    if (this.player.progress >= 800){
       this.sound.play('complete');
       this.player.level += 1;
       this.levelText.setText("Level: "+this.player.level);
@@ -145,7 +142,8 @@ if (this.player.progress >= 800){
       this.player.progressl = 800;
       localStorage.setItem('progress',this.player.progress);
       localStorage.setItem('progressl',this.player.progressl);
-    };
+    }
+	};
 	this.die = ()=>{
 		this.physics.pause();
     this.player.score-=100;
