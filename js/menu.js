f7c840ab3f7b0c3f136b16a6641d3dcc9ca8b6f2
Grant McNamara <Grantmac12345678911@outlook.com>
2021-10-15 07:50:10 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -26,6 +26,9 @@ var Menu = new Phaser.Class({
     this.clickDontFall = this.add.text(220, 280, "Don't Fall!", { fill: '#0f0'})
      .setInteractive()
      .on('pointerdown', () => {this.scene.start("DFMap")} )
+    this.clickDontFall = this.add.text(290, 220, "Going Up", { fill: '#0f0'})
+     .setInteractive()
+     .on('pointerdown', () => {this.scene.start("GUMap")} )
     },
     update: function() {
     }
